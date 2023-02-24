ce07938a146e29a4c8bfbd2bcf52c0f24f636145
Muhammad Haffad <robottxt5@gmail.com>
2023-02-24 19:58:09 +0700

    Memperbaiki nama class di factory

--- database/factories/OrderItemFactory.php
+++ database/factories/OrderItemFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\OrderItem>
  */
-class OrderDetailFactory extends Factory
+class OrderItemFactory extends Factory
 {
     /**
      * Define the model's default state.
--- database/factories/ProductItemFactory.php
+++ database/factories/ProductItemFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ProductItem>
  */
-class ProductDetailFactory extends Factory
+class ProductItemFactory extends Factory
 {
     /**
      * Define the model's default state.
