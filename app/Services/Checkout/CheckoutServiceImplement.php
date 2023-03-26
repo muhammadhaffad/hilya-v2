243280e3a4332445cf0c9a31ebf8d94644ea16c6
Muhammad Haffad <robottxt5@gmail.com>
2023-03-26 13:30:31 +0700

    try in another

--- app/Services/Checkout/CheckoutServiceImplement.php
+++ app/Services/Checkout/CheckoutServiceImplement.php
@@ -268,8 +268,8 @@ class CheckoutServiceImplement implements CheckoutService
                     'transactiontime' => $transaction['transaction_time']
                 ]);
                 $codeOrder = $checkout->first()->code;
-                ProductOrigin::whereIn('id', [1,2])->update(['stock', 2]);
-                ProductItem::where('id', 1)->update(['stock', 2]);
+                ProductOrigin::whereIn('id', [1,2])->update(['stock' => 4]);
+                ProductItem::where('id', 1)->update(['stock'=> 4]);
                 // $checkout = Order::where('code', $codeOrder);
                 // $orderItems = $checkout->first()->load('orderItems.productItem.productOrigins')->orderItems;
                 // foreach ($orderItems as $orderItem ) {
